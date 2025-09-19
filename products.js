1cf38229bb0544056b2dadcdc567699a662257d7
shahatul51-code <shahatul51@gmail.com>
2025-09-19 18:20:06 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,48 @@
+// data/products.js
+
+const products = [
+  {
+    id: 1,
+    name: 'Snake Plant',
+    price: 15,
+    category: 'Low Light',
+    thumbnail: 'https://picsum.photos/seed/snakeplant/150/150',
+  },
+  {
+    id: 2,
+    name: 'Aloe Vera',
+    price: 12,
+    category: 'Succulent',
+    thumbnail: 'https://picsum.photos/seed/aloevera/150/150',
+  },
+  {
+    id: 3,
+    name: 'Spider Plant',
+    price: 10,
+    category: 'Air Purifying',
+    thumbnail: 'https://picsum.photos/seed/spiderplant/150/150',
+  },
+  {
+    id: 4,
+    name: 'Peace Lily',
+    price: 18,
+    category: 'Air Purifying',
+    thumbnail: 'https://picsum.photos/seed/peacelily/150/150',
+  },
+  {
+    id: 5,
+    name: 'Cactus',
+    price: 8,
+    category: 'Succulent',
+    thumbnail: 'https://picsum.photos/seed/cactus/150/150',
+  },
+  {
+    id: 6,
+    name: 'Fiddle Leaf Fig',
+    price: 25,
+    category: 'Indoor Tree',
+    thumbnail: 'https://picsum.photos/seed/fiddleleaffig/150/150',
+  },
+];
+
+export default products;
